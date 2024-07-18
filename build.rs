0cdd3bdf6f810fe8bdf156e4b66f4406efd08c8e
Cameron Dehning <bedsteler2.0@gmail.com>
2024-07-18 11:26:21 -0500

    build: Rework build script to use less dependency's for faster downloads in ci

--- build.rs
+++ build.rs
@@ -1,81 +1,26 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-const C_SDK_OWNER: &str = "cute-lights";
-const C_SDK_REPO: &str = "c-sdk";
-const C_SDK_VERSION: Option<&str> = None; // None for latest
+const VERSION: &str = "v0.2.2";
 
-#[tokio::main]
-async fn main() {
+fn main() {
     println!("cargo:rerun-if-changed=NULL");
     println!("cargo:rerun-if-changed=build.rs");
     let project_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let project_file = Path::new(&project_dir).join("CuteLight.Sdk.csproj");
-
-    if std::env::var("BUMP_VERSION").unwrap_or("0".to_string()) == "1" {
-        let tag = std::env::var("CARGO_PKG_VERSION").unwrap();
-        let content = std::fs::read_to_string(&project_file).unwrap();
-        let re = regex::Regex::new(r#"<Version>(.*)<\/Version> <!-- ci-tag-anchor -->"#).unwrap();
-
-        // get the first group
-        let new_content = re.replace(
-            &content,
-            format!("<Version>{}</Version> <!-- ci-tag-anchor -->", tag).as_str(),
-        );
-
-        std::fs::write(&project_file, new_content.as_bytes()).unwrap();
-    }
 
     let obj_dir = Path::new(&project_dir).join("obj");
     let natives_dir = obj_dir.join("natives");
 
-    let header_path = natives_dir.join("dist/include").join("cutelight.h");
+    let header_path =
+        download_gh_asset("cute-lights", "c-sdk", "cutelight.h", VERSION, &natives_dir);
 
-    let lib_path = natives_dir.join("dist/lib64").join("libcutelight.so");
+    let _lib_path = download_gh_asset(
+        "cute-lights",
+        "c-sdk",
+        "libcutelight.so",
+        VERSION,
+        &natives_dir,
+    );
 
-    if !header_path.exists() || !lib_path.exists() {
-        if natives_dir.exists() {
-            std::fs::remove_dir_all(&natives_dir).unwrap();
-        }
-        std::fs::create_dir_all(&natives_dir).unwrap();
-        let arc_path = natives_dir.join("cutelight.tar.gz");
-
-        let releases = octocrab::instance()
-            .repos(C_SDK_OWNER, C_SDK_REPO)
-            .releases()
-            .list()
-            .send()
-            .await
-            .unwrap();
-
-        let release = releases
-            .items
-            .iter()
-            .find(|r| {
-                if let Some(version) = C_SDK_VERSION {
-                    r.tag_name == version
-                } else {
-                    true
-                }
-            })
-            .expect("No release found")
-            .assets
-            .iter()
-            .find(|a| a.name == "cutelight.tar.gz")
-            .expect("No asset found");
-
-        let data = reqwest::get(&release.browser_download_url.to_string())
-            .await
-            .unwrap()
-            .bytes()
-            .await
-            .unwrap();
-
-        std::fs::write(&arc_path, data).unwrap();
-
-        let tar = flate2::read::GzDecoder::new(std::fs::File::open(&arc_path).unwrap());
-        let mut archive = tar::Archive::new(tar);
-        archive.unpack(&natives_dir).unwrap();
-    }
     println!("cargo:rerun-if-changed={}", header_path.display());
     let rs_intermediary_path = obj_dir.join("intermediary.rs");
 
@@ -102,4 +47,37 @@ async fn main() {
                 .join("NativeMethods.g.cs"),
         )
         .unwrap();
+}
+
+fn download_gh_asset(
+    owner: &str,
+    repo: &str,
+    asset_name: &str,
+    version: &str,
+    target_dir: &Path,
+) -> PathBuf {
+    let out_file = target_dir.join(asset_name);
+
+    if out_file.exists() {
+        return out_file;
+    }
+
+    if !target_dir.exists() {
+        std::fs::create_dir_all(&target_dir).unwrap();
+    }
+
+    let dl_url = format!(
+        "https://github.com/{}/{}/releases/download/{}/{}",
+        owner, repo, version, asset_name
+    );
+
+    let command = format!("curl -L {} -o {}", dl_url, out_file.to_str().unwrap());
+
+    std::process::Command::new("sh")
+        .arg("-c")
+        .arg(command)
+        .output()
+        .expect("Failed to download asset");
+
+    return out_file;
 }
